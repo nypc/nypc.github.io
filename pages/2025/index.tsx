17435558a367bc4eec157507aeb19b1177312274
Jinho Kim <scudjh@gmail.com>
2025-06-25 21:41:11 +0900

    add 2025/index, 2025/rule

---
+++ pages/2025/index.tsx
@@ -0,0 +1,47 @@
+import { Divider, Itemize, Typo } from "@solved-ac/ui-react";
+import { PostLayout, ProblemList, ProblemListContainer } from "components";
+import type { NextPage } from "next";
+import Link from "next/link";
+
+const year = 2025;
+
+
+const problems = {
+  round1: [
+  ],
+  round2a: [
+  ],
+  round2b: [
+  ],
+  finals1214: [
+  ],
+  finals1519: [
+  ],
+} as const;
+
+const List: NextPage = (props) => {
+  return (
+    <PostLayout
+      meta={{
+        title: `NYPC ${year}`,
+      }}
+      theme={{
+        background: "#181D43",
+        color: "#FFFFFF",
+      }}
+    >
+      <Typo h2>안내</Typo>
+      <Itemize>
+        <li>
+          <Link href="/2025/notice/rule">대회 규칙</Link>
+        </li>
+        <li>
+          <Link href="/2025/notice/tool">개발 도구 사용 안내</Link>
+        </li>
+      </Itemize>
+      <Divider />
+    </PostLayout>
+  );
+};
+
+export default List;
